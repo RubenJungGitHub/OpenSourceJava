e1ca97543364165f5d443d3513ea57a0cfbfd4f4
RubenJungGitHub <ruben.jung@outlook.com>
2026-04-09 16:04:05 +0200

    Fix end-user container dump

--- ils/receiver/src/main/java/contain/opensource/ils/bs/receiver/classes/alfresco/AlfrescoNodeController.java
+++ ils/receiver/src/main/java/contain/opensource/ils/bs/receiver/classes/alfresco/AlfrescoNodeController.java
@@ -414,7 +414,6 @@ public class AlfrescoNodeController {
       // Wrap all model properties inside a "properties" object
       ObjectNode propertiesNode = mapper.createObjectNode();
 
-      propertiesNode.put("contain:BINDIO", true);
       if (IOobject.getTitle() != null)
         propertiesNode.put("cm:title", IOobject.getTitle());
       if (IOobject.getDescription() != null)
