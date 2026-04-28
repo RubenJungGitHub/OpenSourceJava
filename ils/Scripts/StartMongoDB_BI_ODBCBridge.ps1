2e0744270578c4620a2eec3442aedace08e3dce1
RubenJungGitHub <71824094+RubenJungGitHub@users.noreply.github.com>
2026-04-28 20:09:42 +0200

    Fix config

--- ils/Scripts/StartMongoDB_BI_ODBCBridge.ps1
+++ ils/Scripts/StartMongoDB_BI_ODBCBridge.ps1
@@ -1,6 +1,6 @@
 ﻿cls
 & "C:\Program Files\MongoDB\Connector for BI\2.14\bin\mongosqld.exe" `
-  --config "C:\ContainOpenSource\OpenSourceJava\ils\receiver\src\main\resources\bi-config-VM.yaml" `
+  --config "D:\contain\OpenSourceJava\ils\receiver\src\main\resources\bi-config-VM.yaml" `
   --auth `
   --mongo-username "bi_user" `
   --mongo-password "bi_password" `
