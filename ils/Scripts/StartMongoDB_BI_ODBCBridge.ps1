ebe96d91a6482e6bbe4608de53a104b061d1771a
RubenJungGitHub <ruben.jung@outlook.com>
2026-04-28 12:19:46 +0200

    Commit

---
+++ ils/Scripts/StartMongoDB_BI_ODBCBridge.ps1
@@ -0,0 +1,7 @@
+﻿& "C:\Program Files\MongoDB\Connector for BI\2.14\bin\mongosqld.exe" `
+  --config "C:\ContainOpenSource\Java\OpenSourceJava\ils\receiver\src\main\resources\bi-config.yaml" `
+  --auth `
+  --mongo-username "bi_user" `
+  --mongo-password "bi_password" `
+  --mongo-authenticationSource "admin" `
+  --sampleNamespaces "ilstools.*"
